dea9786c0dcd1087c24f5add306242b1f01e476e
WangRuizhi66 <1115418657@qq.com>
2019-11-08 16:47:31 +0800

    second

---
+++ jump7.py
@@ -0,0 +1,4 @@
+wget https://labfile.oss.aliyuncs.com/courses/1330/linux.png
+wget https://labfile.oss.aliyuncs.com/courses/1330/python.1png.png
+wget https://labfile.oss.aliyuncs.com/courses/1330/python.2png.png
+wget https://labfile.oss.aliyuncs.com/courses/1330/git.png
